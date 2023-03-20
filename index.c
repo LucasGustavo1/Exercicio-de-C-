36cef674887e68f36484f52b19ff5d13e7ccf9fa
Lucas Gustavo <lucas_gustavo2016@hotmail.com>
2023-03-20 17:07:59 -0300

    Questão D

--- index.c
+++ index.c
@@ -1,36 +1,40 @@
-Questão 1 
+/*Faça um programa que receba do usuário um vetor com 10 posições. Em seguida deverá ser impresso o maior 
+e o menor elemento do vetor
+
+
+Questão D */
 #include <stdio.h>
 
 int main()
 {
-    int vet[10];
+    int vetor[10];
     int i;
-    int maiorn, menorn = 0;
+    int maior, menor = 0;
     
     printf("Insira os vetores: \n");
     for (i = 0; i <=9; i++){
         printf("%dº: ", i);
-        scanf("%d", &vet[i]);
+        scanf("%d", &vetor[i]);
         
     }
     
-    maiorn = vet[0];
-    menorn = vet[0];
+    maior = vetor[0];
+    menor = vetor[0];
     
     for (i = 1; i <=9; i++){
         
-        if(vet[i] > maiorn ){
-            maiorn = vet[i];
+        if(vetor[i] > maior ){
+            maior = vetor[i];
         }
         
-        if (vet[i] < menorn){
-            menorn = vet[i];
+        if (vetor[i] < menor){
+            menor = vetor[i];
         }
     }
     
     printf("-------------------------------------------");
-    printf("\n\nMaior valor do vetor: %d\n\n", maiorn);
-    printf("Menor valor do vetor: %d\n\n", menorn);
+    printf("\n\nMaior valor do vetor: %d\n\n", maior);
+    printf("Menor valor do vetor: %d\n\n", menor);
 
     return 0;
 }
